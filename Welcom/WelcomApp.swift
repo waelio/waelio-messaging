92e90a25ff8332d3cdd1ee3ac574c7e5ac59d510
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2026-03-07 14:01:47 +0200

    refactor: remove Info.plist and update background modes in project settings

--- Welcom/WelcomApp.swift
+++ Welcom/WelcomApp.swift
@@ -6,16 +6,12 @@
 //
 
 import SwiftUI
-import CoreData
 
 @main
 struct WelcomApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
