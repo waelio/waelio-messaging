e708ca3be85ac3a99641faba467a66c8c6fa4f7c
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2026-03-28 13:41:26 +0300

    chore: ignore dist/ and public/ build artifacts

--- dist/server.d.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import http from 'http';
-declare const app: import("express-serve-static-core").Express;
-declare const server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
-export { app, server };
-export declare function startServer(): Promise<void>;
